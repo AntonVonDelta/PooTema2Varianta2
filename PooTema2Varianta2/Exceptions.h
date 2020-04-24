bdcb97a5e5d746ee87dcf0097b43db0ae7df9be3
Reznicencu Sergiu <sergiureznicencu@gmail.com>
2020-04-24 12:30:51 +0300

    Added  empty but mandatory copy constructors to the exceptions

--- PooTema2Varianta2/Exceptions.h
+++ PooTema2Varianta2/Exceptions.h
@@ -5,10 +5,18 @@
 
 class ExceptionOutOfMemory :public std::exception {
 public:
+    ExceptionOutOfMemory() noexcept {}
+    ExceptionOutOfMemory(const ExceptionOutOfMemory&) noexcept = default;
+    ExceptionOutOfMemory& operator= (const ExceptionOutOfMemory&) noexcept = default;
+    virtual ~ExceptionOutOfMemory() noexcept = default;
     const char* what() const throw();
 };
 
 class NotImplemented : public std::exception {
 public:
+    NotImplemented() noexcept {}
+    NotImplemented(const NotImplemented&) noexcept = default;
+    NotImplemented& operator= (const NotImplemented&) noexcept = default;
+    virtual ~NotImplemented() noexcept = default;
     const char* what() const throw();
 };
